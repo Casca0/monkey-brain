b0ec55c6954977562eeea9e18f70a4533bc58536
Lucas Borzani <lucashborzani@gmail.com>
2021-07-15 03:47:14 -0300

    feat/introduced some fun commands

---
+++ commands/boldini.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'boldini',
+  execute(message) {
+    const embed = new MessageEmbed()
+      .setTitle('BOLDINI FUTANARI')
+      .setColor('#fc03a5')
+      .setImage('https://i.imgur.com/BSvdkhZ.png');
+    message.channel.send(embed);
+  },
+};
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,13 +5,14 @@ module.exports = {
   execute(member) {
     const guild = member.guild;
     const newUsers = new Collection();
+    const emoji = member.guild.emojis.cache.find(emj => emj.name == 'Cascamemes');
     newUsers.set(member.id, member.user);
     const defaultChannel = guild.channels.cache.get('808458807209361438');
     const userList = newUsers.map(u => u.toString()).join(' ');
     const embed = new MessageEmbed()
-      .setColor('##bdba2a')
+      .setColor('#f0f714')
       .setTitle('Iniciativa Mammus')
-      .setDescription('Bem-vindo(a) macaquinho(a) tome uma banana\nMe dê uma beijoca!\n' + userList)
+      .setDescription(`Bem-vindo(a) macaquinho(a) tome uma banana 🍌\nMe dê uma beijoca! ${emoji}\n` + userList)
       .setThumbnail(member.user.avatarURL());
     defaultChannel.send(embed);
   },
