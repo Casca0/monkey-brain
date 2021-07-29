a06459cb92e8cac3cbac77bc3a62f4dae0de554d
Casca0 <66306188+Casca0@users.noreply.github.com>
2021-07-29 12:50:09 +0000

    fix/fixed a command

--- commands/fun/macetancia.js
+++ commands/fun/macetancia.js
@@ -1,21 +1,22 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
   name: 'macetancia',
   async execute(message) {
     await message.guild.members.fetch();
     const user = message.guild.members.cache.random().user;
-    const m = `VOCÊ ACABA DE MACETAR ${user}`
-
-    if (user) {
-      message.channel.send(m);
-    }
+    let m = `VOCÊ ACABA DE MACETAR ${user}`
 
     if (user == message.author) {
       m = 'CARALHO!\nVOCÊ SE MACETOU!!!';
       message.channel.send(m);
     }
     else if (user.id == '840221907622166579') {
-      m = 'TU TENTOU ME MACETAR?\nO FILHO DA PUTA, VOU TE TACAR A BANANA EM';
-      message.channel.send(m);
+      const embed = new MessageEmbed()
+        .setTitle('Você tentou me macetar?')
+        .setColor('#967927')
+        .setImage('https://i.imgur.com/mWw7OIa.gif');
+      message.channel.send(embed);
     }
     else if (user.bot == true) {
       m = 'TU TENTOU MACETAR UM BOT E TU FOI MACETADO';
@@ -25,5 +26,9 @@ module.exports = {
       m ='VOCÊ MACETOU O ADM, QUE FILHO DA PUTA!';
       message.channel.send(m);
     }
-  },
+
+    else {
+      message.channel.send(m);
+    }
+  }, 
 };
--- commands/moderation/prune.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//   name:'prune',
-//   execute(message) {
-//     message.guild.members.prune({ days: 1, dry: true })
-//       .then(pruned => console.log(`This will prune ${pruned} people!`));
-//   },
-// };
