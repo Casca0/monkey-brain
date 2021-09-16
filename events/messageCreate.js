5d4c4be12d7154391f2f83592c1546b6ad68ad2f
Lucas Borzani <lucas.borzani@enext.com.br>
2021-09-16 19:50:33 -0300

    fixed alias test

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,7 +11,7 @@ module.exports = {
 
     if (!client.commands.has(commandName)) return;
 
-    const command = client.commands.get(commandName) || client.aliases.get(commandName);
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
     // Command cooldown
 
