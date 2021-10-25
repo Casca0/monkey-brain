04391e8ec84f9ad67712c47600b16475967a8c08
Casca0 <66306188+Casca0@users.noreply.github.com>
2021-10-25 10:33:11 -0300

    Changed user ID type in items model (#7)

--- models/userItems.js
+++ models/userItems.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userItems = new mongoose.Schema({
-  user_id: { type: Number, require: true, unique: true },
+  user_id: { type: String, require: true, unique: true },
   item_id: { type: Number, require: true, unique: true, default: '' },
   item_name: { type: String },
   amount: { type: Number, default: 0 },
