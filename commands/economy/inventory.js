0e9cd3ea78080de260905311dac8efd8896e9d19
Lucas Borzani <lucas.borzani@enext.com.br>
2022-01-06 23:03:38 -0300

    fixed a bug in inventory

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -7,8 +7,11 @@ module.exports = {
   usage: '?inventario',
   async execute(message, profileData, args) {
     try {
-      const all = await items.find({ user_id: message.author.id });
-      const inv = all.map(i => `${i.amount} ${i.item_name.charAt(0).toUpperCase() + i.item_name.slice(1)}`).join(', ');
+      const userItems = await items.find({ user_id: message.author.id });
+      if (!userItems) {
+        message.reply('Seu inventário está vazio!');
+      }
+      const inv = userItems.map(i => `${i.amount} ${i.item_name.charAt(0).toUpperCase() + i.item_name.slice(1)}`).join(', ');
       message.reply(inv);
     }
     catch (err) {
