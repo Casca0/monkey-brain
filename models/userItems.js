6eea036abdb8d596a9396d0a7deeb6b62fd9b77b
Lucas Borzani <lucas.borzani@enext.com.br>
2022-01-07 16:06:01 -0300

    fixed item schema

--- models/userItems.js
+++ models/userItems.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userItems = new mongoose.Schema({
   user_id: { type: String, require: true },
-  item_id: { type: Number, require: true, unique: true, default: '' },
+  item_id: { type: Number, require: true, default: '' },
   item_name: { type: String },
   item_useDescription: { type: String },
   amount: { type: Number, default: 0 },
