3446d4a7315a434637055d8b3b5b3d3ef3661843
Casca0 <lucashborzani@gmail.com>
2022-08-24 00:33:31 -0300

    feat: added context menu for professions selection

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -22,15 +22,82 @@ module.exports = {
     defaultChannel.send({ embeds: [em] });
 
     // Database
+		let profileData;
+    try {
+      profileData = await profileModel.findOne({ userID: member.id });
+      if (!profileData) {
+        const profile = await profileModel.create({
+          userID: member.id,
+          serverID: member.guild.id,
+          coins: 1000,
+          bank: 0,
+          macetanciaCounter: 0,
+          walletName: 'Extrato',
+        });
+        profile.save();
+      }
+    // eslint-disable-next-line brace-style
+    } catch (err) {
+      console.log(err);
+    }
 
-    const profile = await profileModel.create({
-      userID: member.id,
-      serverID: member.guild.id,
-      coins: 1000,
-      bank: 0,
-      macetanciaCounter: 0,
-      walletName: 'Extrato',
-    });
-    profile.save();
+		// Handler de cargo
+
+		try {
+			const rolesMenu = new Discord.MessageActionRow().addComponents(
+				new Discord.MessageSelectMenu()
+					.setCustomId('select')
+					.setPlaceholder('Escolha o seu cargo')
+					.addOptions([
+						{
+							label: 'Desempregado',
+							emoji: '🤐',
+							description: 'Explore o servidor e decida qual profissão seguir!',
+							value: '1010012238750437396',
+						},
+						{
+							label: 'Setor Operacional',
+							emoji: '🧑‍🏭',
+							description: 'Seja um operário dentro da maior corporação de macacos!',
+							value: '1003831889162936381',
+						},
+						{
+							label: 'Setor de RH',
+							emoji: '🧑‍💻',
+							description: 'Trabalhe com recursos humanos em uma corporação que não tem humanos!',
+							value: '1003831494487314432',
+						},
+						{
+							label: 'Setor Comercial',
+							emoji: '🧑‍💼',
+							description: 'Nos ajude à alcançar ainda mais macacos!',
+							value: '1003831729863266325',
+						},
+						{
+							label: 'Setor Financeiro',
+							emoji: '💰',
+							description: 'Nos ajude à evoluir a nossa corporação com a nossa distribuição de recursos!',
+							value: '1003830819648970812',
+						},
+					]),
+			);
+			const professionEmbed = new Discord.MessageEmbed({
+				title: 'Bem-vindo(a) à Mammus Corp.!',
+				description: 'Para continuar dentro da corporação, selecione um cargo!',
+				color: '#40ff66',
+			});
+			const rolesMessage = await member.send({ embeds: [professionEmbed], components: [rolesMenu], ephemeral: true });
+			const collector = rolesMessage.createMessageComponentCollector();
+			collector.on('collect', async interaction => {
+				const user = member.guild.members.resolve(member);
+				const role = member.guild.roles.cache.get(interaction.values[0]);
+				user.roles.add(role).catch(console.error);
+				await interaction.update({ components: [] });
+			});
+		}
+		catch (err) {
+			console.log(err);
+			return;
+		}
   },
 };
